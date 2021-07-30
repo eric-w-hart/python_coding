299c745a4809fb3af32eeef771d66fc8d08680fa
Eric Hart <55008070+eric-w-hart@users.noreply.github.com>
2021-07-30 15:22:44 -0400

    Create bird_in_flight.py
    simple coding example using matplotlib to draw what looks like a bird flying away.

---
+++ drawing/bird_in_flight.py
@@ -0,0 +1,10 @@
+# using plotting, create a pattern representing a flying bird
+import matplotlib.pyplot as plt
+
+# use plot coordinates with opposite patterns and an alternating sequence
+x = [1,3,5,3,7,9]
+y = [9,7,3,5,3,1]
+
+# draw the plot of the bird in flight
+plt.plot(x,y)
+plt.show()
